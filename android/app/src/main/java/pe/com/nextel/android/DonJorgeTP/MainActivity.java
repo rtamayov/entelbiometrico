fd8f506eea968500657e959b24027183688a27c4
rcanales <rodrigocanales09@gmail.com>
2019-01-07 12:38:47 -0500

    Run ok

---
+++ android/app/src/main/java/pe/com/nextel/android/DonJorgeTP/MainActivity.java
@@ -0,0 +1,15 @@
+package pe.com.nextel.android.DonJorgeTP;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import eikon.biometrico.entel.pe.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
