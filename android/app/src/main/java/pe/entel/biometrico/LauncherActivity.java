c20e3f887c13aeda266550fe58dbdea2280c7900
rcanales <rodrigocanales09@gmail.com>
2019-01-08 12:35:29 -0500

    Digital Morpho Merge

--- android/app/src/main/java/pe/entel/biometrico/LauncherActivity.java
+++ android/app/src/main/java/pe/entel/biometrico/LauncherActivity.java
@@ -7,6 +7,11 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.zy.lib.morpho.ui.BioCapture;
+import com.zy.lib.morpho.ui.IBioCapture;
+import com.zy.lib.morpho.ui.ZyRequest;
+import com.zy.lib.morpho.ui.ZyResponse;
+
 import biometrico.entel.pe.R;
 
 public class LauncherActivity extends Activity {
@@ -29,6 +34,45 @@ public class LauncherActivity extends Activity {
 
         m_morpho = (Button) findViewById(R.id.launch_morpho);
 
+        m_morpho.setOnClickListener(new View.OnClickListener()
+        {
+
+            public void onClick(View v)
+            {
+                initializeMorpho();
+            }
+        });
+
     }
 
+
+
+
+    private void initializeMorpho() {
+        IBioCapture iBioCapture = new BioCapture(this, new IBioCapture.ICallback() {
+            @Override
+            public void onStart() {}
+
+            @Override
+            public void onComplete() {}
+
+            @Override
+            public void onSuccess(ZyResponse zyResponse)  {
+
+            }
+
+            @Override
+            public void onError(ZyResponse obj) {
+
+                Toast.makeText(getApplicationContext(), obj.deError,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        ZyRequest zyRequest = new ZyRequest();
+        iBioCapture.capturar(zyRequest);
+
+        Toast.makeText(this.getApplicationContext(), "Initialized",
+                Toast.LENGTH_SHORT).show();
+    }
 }
